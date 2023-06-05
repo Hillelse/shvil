8b468934e4b4ab52b2f3f1e7df8a7ad9155fa0df
unknow <hilell.berros@gmail.com>
2023-06-05 14:01:21 +0300

    add copy button

--- popup.js
+++ popup.js
@@ -5,41 +5,54 @@ let lastRow = document.getElementById("lastRow");
 let lsfr = localStorage.getItem("ShvilFirstRow");
 let lslr = localStorage.getItem("ShvilLastRow");
 
+let issuesStr = '';
+
 if (lsfr != undefined) {
-	firstRow.value = lsfr;
+firstRow.value = lsfr;
 }
 
 document.getElementById("btn").addEventListener("click", async function (e) {
-	localStorage.setItem("ShvilFirstRow", firstRow.value);
-	
+localStorage.setItem("ShvilFirstRow", firstRow.value);
+
   let queryOptions = { active: true, currentWindow: true };
   let tabs = await chrome.tabs.query(queryOptions);
 
   chrome.tabs.sendMessage(
     tabs[0].id,
     { firstRow: firstRow.value,
-		lastRow: lastRow.value
-	},
+lastRow: lastRow.value
+},
     function (response) {
         if (response && response.issues && response.issues.length > 0) {
-			let output = '';
-			response.issues.forEach((currentValue, index, arr) => {
-				output += currentValue + '\n';
-			});
-			//display.innerHTML = 'copied '+ response.issues.length +' issues to clipboard';
-			//navigator.clipboard.writeText(output);
-			copyToClipboard(output);
-			M.toast({html: 'copied '+ response.issues.length +' issues to clipboard', classes:'rounded'});
-		}
+let output = '';
+response.issues.forEach((currentValue, index, arr) => {
+output += currentValue + '\n';
+});
+
+issuesStr = output;
+display.innerHTML = 'find '+response.issues.length + ' issues';
+
+if (document.getElementById("copyPlace").childNodes.length === 0) {
+let copyButton = document.createElement("BUTTON");
+copyButton.classList.add("waves-effect");
+copyButton.classList.add("waves-light");
+copyButton.classList.add("btn");
+copyButton.textContent = 'copy';
+copyButton.onclick = ()=>{
+copyToClipboard();
+};
+document.getElementById("copyPlace").appendChild(copyButton);
+}
+
+}
     }
   );
 });
 
-async function copyToClipboard(msg) {
-	console.log(msg);
+async function copyToClipboard() {
   try {
-    await window.navigator.clipboard.writeText(msg);
-    console.log('copied to clipboard');
+    await window.navigator.clipboard.writeText(issuesStr);
+M.toast({html: 'copied', classes:''});
   } catch (err) {
     console.error('Failed to copy: ', err);
   }
